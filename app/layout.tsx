4aa39805f38ec58fcb7f5fe2d19952648784362b
crvd <casparvandam@hotmail.com>
2026-01-09 21:12:29 +0100

    body layout styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import styles from "./layout.module.css";
 import Header from "@/app/components/header/header";
 import Footer from "@/app/components/footer/footer";
 
@@ -15,10 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={styles.body}>
        <Header />
        
+       <div className={styles.site_content}>
         {children}
+       </div>
 
         <Footer />
       </body>
